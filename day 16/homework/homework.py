7505daa088d4c22fd50403fb850ee0eb186b779e
aleqsandreapkhazava <161210036+aleqsandreapkhazava@users.noreply.github.com>
2024-05-27 11:09:57 +0400

    Delete day 16/homework directory

--- day 16/homework/homework.py
+++
@@ -1,7 +0,0 @@
-for num in range(120,500,3):
-    if num % 3 ==0:
-        print(str(num)+ "your number can be divided by three")
-    if num % 5 ==0:
-        print(str(num) + "your number can be divided by five")
-    if num % 7 ==0:
-        print(str(num) + "your number can be divided by seven")
